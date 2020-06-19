2c99c5d61956c488b870ed57afb19576e6dfd03d
unknown <975569220@22.com>
2020-06-20 01:01:01 +0800

    初始化、配置全局样式、路由、elementui

---
+++ src/views/Login/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id = "login">111<el-button>默认按钮</el-button></div>
+</template>
+
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+#login {
+  height:100vh;
+  background-color:#344a5f;
+}
+</style>
